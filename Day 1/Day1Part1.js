215c6f2f56ba14cb6a4162ba8f6f91bf9b97c895
DSQuentin <dsquentin@hotmail.fr>
2021-12-02 00:56:29 +0100

    feat: changing day 1 part 1 solution using reduce

--- Day 1/Day1Part1.js
+++ Day 1/Day1Part1.js
@@ -5,15 +5,7 @@ var data = fs.readFileSync('./input.txt').toString().split('\n').map(function (i
   return parseInt(item, 10);
 })
 
-function getMeasurements(data) {
-  let cpt = 0
-  for (let i = 0; i < data.length; i++) {
-    // On check si i-1 existe et si i-1 < i, si oui on incrémente le cpt
-    if (data[i - 1] && data[i - 1] < data[i]) {
-      cpt += 1;
-    }
-  }
-  return cpt;
-}
+// reducer = (variable, current, iterator, array)
+const reducer = (total, currentValue, i, data) => data[i - 1] < currentValue ? total + 1 : total;
+console.log(data.reduce(reducer, 0)) //Le 0 => indice de départ
 
-console.log(getMeasurements(data))
